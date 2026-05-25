37a84f716090afa06ef82920e6facbdfaf8fed08
prajukta-panda <prajuktapanda2004@gmail.com>
2026-05-25 22:52:05 +0530

    fix for gunicorn

--- app.py
+++ app.py
@@ -131,9 +131,9 @@ def success():
     )
     mail.send(msg)
     return jsonify({"message": "Payment successful, email sent"})
-
+with app.app_context():
+        db.create_all()
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+   
     app.run(host="0.0.0.0", port=5000)
